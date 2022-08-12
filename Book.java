718e4f9380a7a27ee5b5285f0d5fae073198721b
SOUMYA BANK <soumyabank1996@gmail.com>
2022-08-12 18:47:05 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,50 @@
+package bookManagementSystem;
+
+public class Book {
+		private int bookId;
+		private String bookName;
+		private double bookPrice;
+		
+		public int getBookId()
+		{
+			return bookId;
+		}
+		public void setBookId(int bookId)
+		{
+			this.bookId = bookId;
+		}
+		public String getBookName() 
+		{
+			return bookName;
+		}
+		public void setBookName(String bookName)
+		{
+			this.bookName = bookName;
+		}
+		public double getBookPrice() 
+		{
+			return bookPrice;
+		}
+		public void setBookPrice(double bookPrice) 
+		{
+			this.bookPrice = bookPrice;
+		}
+		public Book(int bookId, String bookName, double bookPrice)
+		{
+			super();
+			this.bookId = bookId;
+			this.bookName = bookName;
+			this.bookPrice = bookPrice;
+		}
+		public Book()
+		{
+			super();
+		}
+		
+		@Override
+		public String toString()
+		{
+			return "Book [bookId=" + bookId + ", bookName=" + bookName + ", bookPrice=" + bookPrice + "]";
+		}
+		
+	}
